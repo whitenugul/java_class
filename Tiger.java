5e9455cb5c6c34e8ec89b475af6932359aa32d75
whitenugul <andrewahn00@gmail.com>
2021-07-29 21:10:35 +0900

    Create Tiger.java

---
+++ Tiger.java
@@ -0,0 +1,11 @@
+public class Tiger extends Animal implements Predator{
+    public String getFood(){
+        return "apple";
+    }
+    public String getDring(){
+        return "something";
+    }
+    public String sayHi(){
+        return "ss";
+    }
+}
